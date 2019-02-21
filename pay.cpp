07f23f4f559eeb8ba5d742908bb2b0bf4f2089f5
NicWebster96 <NicWebster96@gmail.com>
2019-02-21 12:06:15 -0800

    added files

---
+++ pay.cpp
@@ -0,0 +1,45 @@
+// Created by Nicholas Webster
+
+#include "person.cpp"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+void readData(Person a[], int size) {
+
+  string lName, fName;
+  float pRate, hWorked;
+  int i = 0;
+
+  ifstream myFile("input.txt");
+
+  while(myFile >> fName >> lName >> hWorked >> pRate) {
+    a[i].setFirstName(fName);
+    a[i].setLastName(lName);
+    a[i].setHoursWorked(hWorked);
+    a[i].setPayRate(pRate);
+    i++;
+  }
+}
+
+void writeData(Person a[], int size) {
+
+  ofstream myFile("output.txt");
+
+  for (int i = 0; i<size; i++) {
+    if (a[i].fullName() == " ") {
+      break;
+    }
+    myFile << a[i].fullName() << " ";
+    myFile << fixed << setprecision(2) << a[i].totalPay() << endl;
+  }
+}
+
+int main() {
+
+  Person emp[20];
+  readData(emp, 20);
+  writeData(emp, 20);
+
+}
---
+++ person.cpp
@@ -0,0 +1,31 @@
+// Created by Nicholas Webster
+
+#include "person.h"
+using namespace std;
+
+Person::Person() {
+  lastName = "";
+  firstName = "";
+  payRate = 0.0;
+  hoursWorked = 0.0;
+}
+
+void Person::setLastName(string lName) { lastName = lName; }
+
+string Person::getLastName() { return lastName; }
+
+void Person::setFirstName(string fName) { firstName = fName; }
+
+string Person::getFirstName() { return firstName; }
+
+void Person::setPayRate(float rate) { payRate = rate; }
+
+float Person::getPayRate() { return payRate; }
+
+void Person::setHoursWorked(float hrsWorked) { hoursWorked = hrsWorked; }
+
+float Person::getHoursWorked() { return hoursWorked; }
+
+float Person::totalPay() { return hoursWorked * payRate; }
+
+string Person::fullName() { return firstName + " " + lastName; }
